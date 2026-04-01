bdb50b45467dbfb941394a75cdc61fe675e4507d
Italbatterie <batteriefier@gmail.com>
2026-04-01 15:53:41 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,8 +1,5 @@
-// ─────────────────────────────────────────────
-//  ITALBATTERIE — Configurazione
-//  Modifica questo file per aggiornare l'URL
-//  dello script Google Apps Script.
-// ─────────────────────────────────────────────
+// ITALBATTERIE - Configurazione
+// Modifica SHEETS_URL dopo ogni nuova distribuzione dello script Apps Script.
 
 var IB_CONFIG = {
   SHEETS_URL: 'https://script.google.com/macros/s/AKfycbzozBZl49FkMiNe5kFpV1bI_hD8sKt1nF8zbL5MxIgdND-Z3TTP_YBZ8umuuJwn6DXxgg/exec'
